064b93fc08e5efab42bc71201c1bf12f026143a9
enzosalmon1 <enzosalmon1@gmail.com>
2021-06-14 10:13:32 +0200

    Player animation when fireball launch (without fireball)

---
+++ Assets/Player/PlayerSpells.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerSpells : NetworkBehaviour
+{
+    public GameObject PlayerLocal;
+    public GameObject fireballLaunch;
+    public Transform fireballSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer) { return; };
+
+        if (Input.GetKey("a"))
+            {
+            PlayerLocal.GetComponent<Animator>().Play("Fireball", -1, 0f);
+           // Fireball();
+            }
+            //else { PlayerLocal.GetComponent<Animator>().Play("Fireball", -1, 0f);}
+    }
+   
+}
